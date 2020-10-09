bd47148a5bfc1a08af31dc96fc11fe10996c6c51
quydang <hadesi1999@gmail.com>
2020-10-09 14:31:36 +0700

    Define all models

---
+++ app/Models/Lesson.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDelete;
+
+class Lesson extends Model
+{
+    use SoftDelete;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'video_url',
+        'course_id',
+        'course_order',
+    ];
+
+    protected $dates = ['deleted_at'];
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'lesson_user');
+    }
+
+    public function exercises()
+    {
+        return $this->hasMany(Exercise::class);
+    }
+
+    public function requests()
+    {
+        return $this->hasMany(Request::class);
+    }
+
+    public function comments()
+    {
+        return $this->morphMany(Comment::class, 'commentable');
+    }
+
+    public function course()
+    {
+        return $this->belongsTo(Course::class);
+    }
+}
---
+++ app/Models/Request.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Request extends Model
+{
+    protected $fillable = [
+        'student_id',
+        'lesson_id',
+        'status',
+        'mentor_id',
+    ];
+
+    public function lesson()
+    {
+        return $this->belongsTo(Lesson::class);
+    }
+
+    public function mentor()
+    {
+        return $this->belongsTo(User::class, 'mentor_id');
+    }
+
+    public function student()
+    {
+        return $this->belongsTo(User::class, 'student_id');
+    }
+}
