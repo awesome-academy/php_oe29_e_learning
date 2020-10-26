50f107c15b9f805c1cc798227e4cd01b9a9a958c
quydang <hadesi1999@gmail.com>
2020-10-26 09:00:28 +0700

    auth controller setup

---
+++ database/migrations/2020_10_26_011040_modifier_attribute_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ModifierAttributeUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('phone')->nullable()->change();
+            $table->integer('date_of_birth')->nullable()->change();
+            $table->string('address')->nullable()->change();
+            $table->string('github_url')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('phone')->change();
+            $table->integer('date_of_birth')->change();
+            $table->string('address')->change();
+            $table->string('github_url')->change();
+        });
+    }
+}
